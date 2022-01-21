db741a721891c4c26d8b8cb53d8e68e5991527a7
Ilya <sakuru1x1@gmail.com>
2022-01-21 13:42:37 +0300

    Added program logic

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,13 @@
         <div class="tile tile-four" @click="tileClick"></div>
       </div>
       <div class="diff-selector">
-        <h2>Уровень сложности</h2>
+        <h2>Счет:{{this.score}}</h2>
+        <h3>Уровень сложности</h3>
         <input type="radio" id="easy" name="diff" v-model="difficulty" value="1500"><label for="easy" >Легко</label>
-        
         <input type="radio" id="med" name="diff" v-model="difficulty" value="1000"><label for="med">Средне</label>
         <input type="radio" id="high" name="diff" v-model="difficulty" value="400"><label for="high">Сложно</label>
       </div>
-      <div class="btn" @click="playGame">Play</div>
+      <div class="btn" @click="playGame">Играть</div>
     </div>
   </div>
 </template>
@@ -26,7 +26,7 @@ export default {
   name: 'App',
   data(){
     return {
-      difficulty:'',
+      difficulty:0,
       sounds: [
       new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
       new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
@@ -34,46 +34,88 @@ export default {
       new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
       )
     ],
+      isRound:false,
       isPlaying:false,
       tilesList:[],
-      tileToClick:0
+      tileToClick:0,
+      tileToClickIndex:0,
+      roundCount:1,
+      score:0
     }
   },
   components: {
     
   },
   methods:{
-   playTile(tile,index){
-       tile.classList.toggle(tile.classList[1]+'-light');
-       this.sounds[index].play();
-       setTimeout(()=> {
-         tile.classList.toggle(tile.classList[1]+'-light');       
-       }, +this.difficulty);
+   playTile(index,delay){ // Проигрывание одной клетки
+     const colorTiles = document.querySelectorAll('.tile')
+     
+    setTimeout(() => {
+     
+      colorTiles[index].classList.add(colorTiles[index].classList[1] + '-light');
+      this.sounds[index].play();
+      setTimeout(() => {
+        
+        colorTiles[index].classList.remove(colorTiles[index].classList[1] + '-light');
+
+      }, this.difficulty)
+    },delay)
   },
-  getTile(){
-    if(!this.tilesList.length){
-      this.tilesList.push(Math.round(Math.random() * 3));
+  playRound(round){ // Проигрывание раунда
+    let delay = +this.difficulty;
+    let random = Math.floor(Math.random() * 4);
+    this.tilesList.push(random);
+  for(let i = 0; i < round; i++){
+    this.playTile(this.tilesList[i],delay);
+    delay += +this.difficulty + 500;
+
+    if(i === round - 1){
+      setTimeout(() => {
+         this.isRound = false;
+      },delay)
     }
-    const tiles = document.querySelectorAll('.tile');
-    this.playTile(tiles[this.tilesList[0]],this.tilesList[0],this.difficulty);
-    
+  }
+    this.roundCount++;
+    this.tileToClick = this.tilesList[0];
+    this.tileToClickIndex = 0;
   },
-  playGame(){
-      this.getTile();
+  playGame(){// Старт игры
+      if(!this.difficulty || this.isRound) return;
+      this.isRound = true;
+      this.isPlaying = true;
+      this.playRound(this.roundCount);
     },
     tileClick(e){
       const tiles = Array.from(document.querySelectorAll('.tile'));
-      console.log(this.tileToClick)
-      if(this.tileToClick === tiles.indexOf(e.target)){
+      if((tiles.indexOf(e.target) === this.tileToClick )&& !this.isRound && this.isPlaying){
+        tiles[this.tileToClick].classList.add(tiles[this.tileToClick].classList[1] + '-light');
+        this.sounds[this.tileToClick].play();
+        setTimeout(() => {
+        
+        tiles[this.tileToClick].classList.remove(tiles[this.tileToClick].classList[1] + '-light');
 
-      e.target.classList.toggle(e.target.classList[1]+'-light');
-       this.sounds[tiles.indexOf(e.target)].play();
-       setTimeout(()=> {
-         e.target.classList.toggle(e.target.classList[1]+'-light');       
-       }, 1500);
+      }, 300)
+        this.score++;
+        if(this.tileToClickIndex < this.tilesList.length - 1){
+          this.tileToClick = this.tilesList[this.tileToClickIndex + 1];
+          this.tileToClickIndex += 1;
+        }else{
+          setTimeout(() => {this.playRound(this.roundCount)},this.difficulty);
+        }
+      }else if(this.isRound){
+          return;
+        }else if(this.isPlaying){
+        alert(`Игра окончена, счет:${this.score}`);
+        this.score = 0;
+        this.isPlaying = false;
+        this.roundCount = 1;
+        this.tilesList = [];
+        this.tileToClick = 0;
+        this.tileToClickIndex = 0;
+      }
+      
       }
     }
-  }
 }
 </script>
 
